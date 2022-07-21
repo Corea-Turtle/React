0bcf0dd0742512b97e32e6539fb4d346672b14da
Corea-Turtle <eastino@gmail.com>
2022-07-21 16:41:40 +0900

    init v0.0.1

---
+++ CH00_준비하기/Javascript/ex01_console.js
@@ -0,0 +1,7 @@
+console.log("Hello React!!!");
+//자바 스크립트는 동적 타입 언어이다. 미리 자료형이 정해진게 아니라 실행시 자료형 판단 후 세팅한다.
+var a=1,b=2,d="string"
+console.log(a,b,d);
+
+console.log(typeof(d));
+console.log("값은 %d 입니다.",a);
